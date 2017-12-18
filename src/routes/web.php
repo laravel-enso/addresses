386953e81b587e8c341c3224b2a33d2e78b68e2b
Mihai <mihai.ocneanu@gmail.com>
2017-12-18 17:37:28 +0200

    updated edit/create form endpoints and permissions

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,8 +4,8 @@ Route::middleware(['web', 'auth', 'core'])
     ->namespace('LaravelEnso\AddressesManager\app\Http\Controllers')
     ->group(function () {
         Route::prefix('addresses')->as('addresses.')->group(function () {
-            Route::get('getEditForm/{address}', 'AddressesController@getEditForm')->name('getEditForm');
-            Route::get('getCreateForm', 'AddressesController@getCreateForm')->name('getCreateForm');
+            Route::get('{address}/edit', 'AddressesController@getEditForm')->name('edit');
+            Route::get('create', 'AddressesController@getCreateForm')->name('create');
             Route::get('list', 'AddressesController@list')->name('list');
             Route::get('setDefault/{address}', 'AddressesController@setDefault')->name('setDefault');
             Route::post('/{type}/{id}', 'AddressesController@store')->name('store');
