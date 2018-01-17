aa0e3840d78db443993b8932d4c47ed1c63ccfba
Mihai <mihai.ocneanu@gmail.com>
2018-01-17 11:48:19 +0200

    updated routes

--- src/database/migrations/2017_12_07_150655_create_structure_for_addresses.php
+++ src/database/migrations/2017_12_07_150655_create_structure_for_addresses.php
@@ -5,17 +5,17 @@ use LaravelEnso\StructureManager\app\Classes\StructureMigration;
 class CreateStructureForAddresses extends StructureMigration
 {
     protected $permissionGroup = [
-        'name' => 'addresses', 'description' => 'Addresses group',
+        'name' => 'core.addresses', 'description' => 'Addresses group',
     ];
 
     protected $permissions = [
-        ['name' => 'addresses.update', 'description' => 'Update edited address', 'type' => 1, 'default' => false],
-        ['name' => 'addresses.store', 'description' => 'Store newly created address', 'type' => 1, 'default' => false],
-        ['name' => 'addresses.destroy', 'description' => 'Delete address', 'type' => 1, 'default' => false],
-        ['name' => 'addresses.index', 'description' => 'Get addresses for addressable', 'type' => 0, 'default' => false],
-        ['name' => 'addresses.setDefault', 'description' => 'Set Address as default', 'type' => 1, 'default' => false],
-        ['name' => 'addresses.edit', 'description' => 'Get Edit Form', 'type' => 0, 'default' => false],
-        ['name' => 'addresses.create', 'description' => 'Get Create Form', 'type' => 0, 'default' => false],
-        ['name' => 'addresses.getCountryList', 'description' => 'Get countries option list for select', 'type' => 0, 'default' => false],
+        ['name' => 'core.addresses.update', 'description' => 'Update edited address', 'type' => 1, 'default' => false],
+        ['name' => 'core.addresses.store', 'description' => 'Store newly created address', 'type' => 1, 'default' => false],
+        ['name' => 'core.addresses.destroy', 'description' => 'Delete address', 'type' => 1, 'default' => false],
+        ['name' => 'core.addresses.index', 'description' => 'Get addresses for addressable', 'type' => 0, 'default' => false],
+        ['name' => 'core.addresses.setDefault', 'description' => 'Set Address as default', 'type' => 1, 'default' => false],
+        ['name' => 'core.addresses.edit', 'description' => 'Get Edit Form', 'type' => 0, 'default' => false],
+        ['name' => 'core.addresses.create', 'description' => 'Get Create Form', 'type' => 0, 'default' => false],
+        ['name' => 'core.addresses.countriesSelectOptions', 'description' => 'Get countries option list for select', 'type' => 0, 'default' => false],
     ];
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
 Route::middleware(['web', 'auth', 'core'])
-    ->prefix('api')
+    ->prefix('api/core')->as('core.')
     ->namespace('LaravelEnso\AddressesManager\app\Http\Controllers')
     ->group(function () {
         Route::resource('addresses', 'AddressesController', ['except' => ['show']]);
         Route::patch('addresses.setDefault/{address}', 'AddressesController@setDefault')->name('addresses.setDefault');
-        Route::get('addresses.getCountryList', 'CountriesSelectController@getOptionList')->name('addresses.getCountryList');
+        Route::get('addresses.countriesSelectOptions', 'CountriesSelectController@getOptionList')->name('addresses.countriesSelectOptions');
     });
