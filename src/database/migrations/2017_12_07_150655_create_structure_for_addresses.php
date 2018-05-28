e0f265c46f199d75d840d2ac31e3c54756873e26
mishu <mishu@xtelecom.ro>
2018-05-28 13:19:19 +0300

    updates for the new structure manager

--- src/database/migrations/2017_12_07_150655_create_structure_for_addresses.php
+++ src/database/migrations/2017_12_07_150655_create_structure_for_addresses.php
@@ -9,13 +9,13 @@ class CreateStructureForAddresses extends StructureMigration
     ];
 
     protected $permissions = [
-        ['name' => 'core.addresses.update', 'description' => 'Update edited address', 'type' => 1, 'default' => false],
-        ['name' => 'core.addresses.store', 'description' => 'Store newly created address', 'type' => 1, 'default' => false],
-        ['name' => 'core.addresses.destroy', 'description' => 'Delete address', 'type' => 1, 'default' => false],
-        ['name' => 'core.addresses.index', 'description' => 'Get addresses for addressable', 'type' => 0, 'default' => false],
-        ['name' => 'core.addresses.setDefault', 'description' => 'Set Address as default', 'type' => 1, 'default' => false],
-        ['name' => 'core.addresses.edit', 'description' => 'Get Edit Form', 'type' => 0, 'default' => false],
-        ['name' => 'core.addresses.create', 'description' => 'Get Create Form', 'type' => 0, 'default' => false],
-        ['name' => 'core.addresses.countriesSelectOptions', 'description' => 'Get countries option list for select', 'type' => 0, 'default' => false],
+        ['name' => 'core.addresses.update', 'description' => 'Update edited address', 'type' => 1, 'is_default' => false],
+        ['name' => 'core.addresses.store', 'description' => 'Store newly created address', 'type' => 1, 'is_default' => false],
+        ['name' => 'core.addresses.destroy', 'description' => 'Delete address', 'type' => 1, 'is_default' => false],
+        ['name' => 'core.addresses.index', 'description' => 'Get addresses for addressable', 'type' => 0, 'is_default' => false],
+        ['name' => 'core.addresses.setDefault', 'description' => 'Set Address as is_default', 'type' => 1, 'is_default' => false],
+        ['name' => 'core.addresses.edit', 'description' => 'Get Edit Form', 'type' => 0, 'is_default' => false],
+        ['name' => 'core.addresses.create', 'description' => 'Get Create Form', 'type' => 0, 'is_default' => false],
+        ['name' => 'core.addresses.countriesSelectOptions', 'description' => 'Get countries option list for select', 'type' => 0, 'is_default' => false],
     ];
 }
