2eb33e95a5de9879415bb8dcd5a071b4f912a33b
mishu <mishu@xtelecom.ro>
2018-10-11 08:26:37 +0300

    adds `onDelete` config option / improves `Addressable` trait

--- src/app/Traits/Addressable.php
+++ src/app/Traits/Addressable.php
@@ -3,9 +3,28 @@
 namespace LaravelEnso\AddressesManager\app\Traits;
 
 use LaravelEnso\AddressesManager\app\Models\Address;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 
 trait Addressable
 {
+    public static function bootAddressable()
+    {
+        self::deleting(function ($model) {
+            if (config('enso.addresses.onDelete') === 'restrict'
+                && $model->addresses()->first() !== null) {
+                throw new ConflictHttpException(
+                    __('The entity has addresses and cannot be deleted')
+                );
+            }
+        });
+
+        self::deleted(function ($model) {
+            if (config('enso.addresses.onDelete') === 'cascade') {
+                $model->addresses()->delete();
+            }
+        });
+    }
+
     public function addresses()
     {
         return $this->morphMany(Address::class, 'addressable');
