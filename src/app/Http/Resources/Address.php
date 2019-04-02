9dffc0159fbf59fe95ce0bf6d3c28ecf99929e03
mishu <mishu@xtelecom.ro>
2019-04-02 15:21:38 +0300

    refactor resource

--- src/app/Http/Resources/Address.php
+++ src/app/Http/Resources/Address.php
@@ -9,22 +9,22 @@ class Address extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'                    => $this->id,
-            'country'               => $this->whenLoaded('country', $this->country->name),
-            'administrativeArea'    => $this->administrative_area,
-            'city'                  => $this->city,
+            'id' => $this->id,
+            'country' => optional($this->country)->name,
+            'administrativeArea' => $this->administrative_area,
+            'city' => $this->city,
             'subAdministrativeArea' => $this->sub_administrative_area,
-            'streetType'            => $this->street_type,
-            'street'                => $this->street,
-            'number'                => $this->number,
-            'building'              => $this->building,
-            'entry'                 => $this->entry,
-            'floor'                 => $this->floor,
-            'apartment'             => $this->apartment,
-            'buildingType'          => $this->building_type,
-            'postalArea'            => $this->postal_area,
-            'obs'                   => $this->obs,
-            'isDefault'             => $this->is_default,
+            'streetType' => $this->street_type,
+            'street' => $this->street,
+            'number' => $this->number,
+            'building' => $this->building,
+            'entry' => $this->entry,
+            'floor' => $this->floor,
+            'apartment' => $this->apartment,
+            'buildingType' => $this->building_type,
+            'postalArea' => $this->postal_area,
+            'obs' => $this->obs,
+            'isDefault' => $this->is_default,
         ];
     }
 }
