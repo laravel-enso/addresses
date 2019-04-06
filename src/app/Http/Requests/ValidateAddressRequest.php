a960537a7e98c9bf9e50146ab6c13153af498726
Adrian Ocneanu <aocneanu@gmail.com>
2019-04-06 11:38:44 +0000

    Apply fixes from StyleCI

--- src/app/Http/Requests/ValidateAddressRequest.php
+++ src/app/Http/Requests/ValidateAddressRequest.php
@@ -17,22 +17,22 @@ class ValidateAddressRequest extends FormRequest
     public function rules()
     {
         $rules = [
-            'addressable_id' => 'required',
+            'addressable_id'   => 'required',
             'addressable_type' => 'required|string',
         ];
 
         return $this->method() === 'GET'
             ? $rules
             : $rules + [
-                'street' => 'required',
-                'city' => 'required',
+                'street'     => 'required',
+                'city'       => 'required',
                 'country_id' => 'required',
             ];
     }
 
     private function checkParams()
     {
-        if (! class_exists($this->get('addressable_type'))) {
+        if (!class_exists($this->get('addressable_type'))) {
             throw new AddressException(
                 'The "addressable_type" property must be a valid model class'
             );
