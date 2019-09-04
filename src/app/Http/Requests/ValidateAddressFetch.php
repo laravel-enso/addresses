3816037c897adb0793147ab0714a711cb4595ba0
Mihai <mihai.ocneanu@gmail.com>
2019-09-04 14:38:48 +0300

    removed superfluous validation

--- src/app/Http/Requests/ValidateAddressFetch.php
+++ src/app/Http/Requests/ValidateAddressFetch.php
@@ -3,14 +3,11 @@
 namespace LaravelEnso\Addresses\App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use LaravelEnso\Addresses\app\Exceptions\AddressException;
 
 class ValidateAddressFetch extends FormRequest
 {
     public function authorize()
     {
-        $this->checkParams();
-
         return true;
     }
 
@@ -20,14 +17,5 @@ class ValidateAddressFetch extends FormRequest
             'addressable_id' => 'required',
             'addressable_type' => 'required|string',
         ];
-    }
-
-    private function checkParams()
-    {
-        if (! class_exists($this->get('addressable_type'))) {
-            throw new AddressException(
-                'The "addressable_type" property must be a valid model class'
-            );
-        }
     }
 }
