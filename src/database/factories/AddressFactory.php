36b100c40b8fd4c2405573e2126455ca9297b826
Adrian Ocneanu <aocneanu@gmail.com>
2019-12-27 14:22:42 +0000

    Apply fixes from StyleCI

--- src/database/factories/AddressFactory.php
+++ src/database/factories/AddressFactory.php
@@ -1,11 +1,10 @@
 <?php
 
-use Generator;
 use Faker\Generator as Faker;
 use LaravelEnso\Addresses\app\Models\Address;
 use LaravelEnso\Countries\app\Models\Country;
 
-$factory->define(Address::class, fn(Faker $faker) => [
+$factory->define(Address::class, fn (Faker $faker) => [
     'addressable_id' => $faker->randomKey,
     'addressable_type' => $faker->word,
     'country_id' => Country::inRandomOrder()->first()->id,
