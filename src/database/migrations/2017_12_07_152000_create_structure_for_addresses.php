fe2c8d5a790e24c57d86bce6bddf7f9165042f2b
mishu <mishu@xtelecom.ro>
2020-03-14 18:04:19 +0200

    removes Types from migration

--- src/database/migrations/2017_12_07_152000_create_structure_for_addresses.php
+++ src/database/migrations/2017_12_07_152000_create_structure_for_addresses.php
@@ -1,17 +1,16 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForAddresses extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.addresses.update', 'description' => 'Update edited address', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'core.addresses.store', 'description' => 'Store newly created address', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'core.addresses.destroy', 'description' => 'Delete address', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'core.addresses.index', 'description' => 'Get addresses for addressable', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'core.addresses.makeDefault', 'description' => 'Make Address as default', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'core.addresses.edit', 'description' => 'Get Edit Form', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'core.addresses.create', 'description' => 'Get Create Form', 'type' => Types::Read, 'is_default' => false],
+        ['name' => 'core.addresses.update', 'description' => 'Update edited address', 'is_default' => false],
+        ['name' => 'core.addresses.store', 'description' => 'Store newly created address', 'is_default' => false],
+        ['name' => 'core.addresses.destroy', 'description' => 'Delete address', 'is_default' => false],
+        ['name' => 'core.addresses.index', 'description' => 'Get addresses for addressable', 'is_default' => false],
+        ['name' => 'core.addresses.makeDefault', 'description' => 'Make Address as default', 'is_default' => false],
+        ['name' => 'core.addresses.edit', 'description' => 'Get Edit Form', 'is_default' => false],
+        ['name' => 'core.addresses.create', 'description' => 'Get Create Form', 'is_default' => false],
     ];
 }
