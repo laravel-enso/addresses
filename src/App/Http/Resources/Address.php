2df05bbaad9236c3c57ba3347da233381a8c171b
Mihai <mihai.ocneanu@gmail.com>
2020-05-22 11:58:13 +0300

    fixed resource for addresses without region and/or locality

--- src/App/Http/Resources/Address.php
+++ src/App/Http/Resources/Address.php
@@ -11,8 +11,8 @@ class Address extends JsonResource
         return [
             'id' => $this->id,
             'country' => $this->relationLoaded('country') ? $this->country->name : null,
-            'region' => $this->relationLoaded('region') ? $this->region->name : null,
-            'locality' => $this->relationLoaded('locality') ? $this->locality->name : null,
+            'region' => $this->relationLoaded('region') ? optional($this->region)->name : null,
+            'locality' => $this->relationLoaded('locality') ? optional($this->locality)->name : null,
             'city' => $this->city,
             'street' => $this->street,
             'additional' => $this->resource->additional,
