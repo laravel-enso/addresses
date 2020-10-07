1431f0ee80631fbc3a96cb725ef62b98c6fbab59
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 15:30:43 +0300

    updates syntax

--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,41 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\Addresses\Http\Controllers\Localities;
+use LaravelEnso\Addresses\Http\Controllers\Regions;
+use LaravelEnso\Addresses\Http\Controllers\Index;
+use LaravelEnso\Addresses\Http\Controllers\Create;
+use LaravelEnso\Addresses\Http\Controllers\Store;
+use LaravelEnso\Addresses\Http\Controllers\Options;
+use LaravelEnso\Addresses\Http\Controllers\Postcode;
+use LaravelEnso\Addresses\Http\Controllers\Edit;
+use LaravelEnso\Addresses\Http\Controllers\Localize;
+use LaravelEnso\Addresses\Http\Controllers\Update;
+use LaravelEnso\Addresses\Http\Controllers\Destroy;
+use LaravelEnso\Addresses\Http\Controllers\Show;
+use LaravelEnso\Addresses\Http\Controllers\MakeDefault;
+use LaravelEnso\Addresses\Http\Controllers\MakeBilling;
+use LaravelEnso\Addresses\Http\Controllers\MakeShipping;
+
 
 Route::middleware(['api', 'auth', 'core'])
     ->prefix('api/core/addresses')->as('core.addresses.')
-    ->namespace('LaravelEnso\Addresses\Http\Controllers')
     ->group(function () {
-        Route::get('localities', 'Localities')->name('localities');
-        Route::get('regions', 'Regions')->name('regions');
-        Route::get('', 'Index')->name('index');
-        Route::get('create', 'Create')->name('create');
-        Route::post('', 'Store')->name('store');
-        Route::get('options', 'Options')->name('options');
-        Route::get('postcode', 'Postcode')->name('postcode');
-        Route::get('{address}/edit', 'Edit')->name('edit');
-        Route::get('{address}/localize', 'Localize')->name('localize');
-        Route::patch('{address}', 'Update')->name('update');
-        Route::delete('{address}', 'Destroy')->name('destroy');
+        Route::get('localities', Localities::class)->name('localities');
+        Route::get('regions', Regions::class)->name('regions');
+        Route::get('', Index::class)->name('index');
+        Route::get('create', Create::class)->name('create');
+        Route::post('', Store::class)->name('store');
+        Route::get('options', Options::class)->name('options');
+        Route::get('postcode', Postcode::class)->name('postcode');
+        Route::get('{address}/edit', Edit::class)->name('edit');
+        Route::get('{address}/localize', Localize::class)->name('localize');
+        Route::patch('{address}', Update::class)->name('update');
+        Route::delete('{address}', Destroy::class)->name('destroy');
 
-        Route::patch('makeDefault/{address}', 'MakeDefault')->name('makeDefault');
-        Route::patch('makeBilling/{address}', 'MakeBilling')->name('makeBilling');
-        Route::patch('makeShipping/{address}', 'MakeShipping')->name('makeShipping');
+        Route::patch('makeDefault/{address}', MakeDefault::class)->name('makeDefault');
+        Route::patch('makeBilling/{address}', MakeBilling::class)->name('makeBilling');
+        Route::patch('makeShipping/{address}', MakeShipping::class)->name('makeShipping');
 
-        Route::get('{address}', 'Show')->name('show');
+        Route::get('{address}', Show::class)->name('show');
     });
