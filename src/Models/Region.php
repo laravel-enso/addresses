84395f54732fad15bd88d6323aaaec358229d45f
mishu <mishu@xtelecom.ro>
2020-11-24 06:15:54 +0200

    adds missing country relation in region

--- src/Models/Region.php
+++ src/Models/Region.php
@@ -3,6 +3,7 @@
 namespace LaravelEnso\Addresses\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use LaravelEnso\Countries\Models\Country;
 use LaravelEnso\Helpers\Contracts\Activatable;
 use LaravelEnso\Helpers\Traits\ActiveState;
 
@@ -11,6 +12,11 @@ class Region extends Model implements Activatable
     use ActiveState;
 
     protected $guarded = ['id'];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
 
     public function localities()
     {
