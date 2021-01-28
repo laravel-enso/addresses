888f078cc46dd1ebe19a71ae9e851b6559a8bacf
reza <raftx24@gmail.com>
2021-01-28 14:39:41 +0330

    use ServiceProvider for register relation instead of Register class

---
+++ src/AddressableServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LaravelEnso\Addresses;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\ServiceProvider;
+use LaravelEnso\Addresses\DynamicRelations\Addressables\Addresss;
+use LaravelEnso\Addresses\DynamicRelations\Addressables\Addressses;
+use LaravelEnso\Addresses\DynamicRelations\Addressables\BillingAddresse;
+use LaravelEnso\Addresses\DynamicRelations\Addressables\ShippingAddresses;
+use LaravelEnso\DynamicMethods\Services\Methods;
+use LaravelEnso\Addresses\Observers\Observer;
+
+class AddressableServiceProvider extends ServiceProvider
+{
+    protected array $register = [];
+
+    public function boot()
+    {
+        Collection::wrap($this->register)
+            ->each(function ($model) {
+                Methods::bind($model, [
+                    Addresss::class, Addressses::class, BillingAddresse::class,
+                    ShippingAddresses::class,
+                ]);
+
+                $model::observe(Observer::class);
+            });
+    }
+}
