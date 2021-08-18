71f2351d2da5ebfd226393de4e50d24adc384039
Mihai <mihai.ocneanu@gmail.com>
2021-08-18 10:12:32 +0300

    improved validation for case when postcode exists in the DB, but for a different country

--- src/Http/Requests/ValidatePostcodeRequest.php
+++ src/Http/Requests/ValidatePostcodeRequest.php
@@ -3,6 +3,8 @@
 namespace LaravelEnso\Addresses\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Collection;
+use LaravelEnso\Addresses\Models\Postcode;
 
 class ValidatePostcodeRequest extends FormRequest
 {
@@ -12,5 +14,25 @@ class ValidatePostcodeRequest extends FormRequest
             'country_id' => 'required|exists:countries,id',
             'postcode' => 'required|exists:postcodes,code',
         ];
+    }
+
+    public function withValidator($validator)
+    {
+        if ($this->postcodeNotFound()) {
+            $attributes = new Collection(['country_id', 'postcode']);
+
+            $validator->after(fn ($validator) => $attributes
+                ->each(fn ($attribute) => $validator->errors()
+                    ->add($attribute, 'Postcode not found')));
+        }
+    }
+
+    private function postcodeNotFound(): bool
+    {
+        return $this->filled('country_id')
+            && $this->filled('postcode')
+            && Postcode::whereCountryId($this->get('country_id'))
+                ->whereCode($this->get('postcode'))
+                ->doesntExist();
     }
 }
