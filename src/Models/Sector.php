ae87c3b695cc2129586710930894195c36de75ae
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2024-10-30 14:55:21 +0200

    makes Sector model rememberable

--- src/Models/Sector.php
+++ src/Models/Sector.php
@@ -3,8 +3,11 @@
 namespace LaravelEnso\Addresses\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use LaravelEnso\Rememberable\Traits\Rememberable;
 
 class Sector extends Model
 {
+    use Rememberable;
+
     protected $guarded = [];
 }
